85ab1ba9a8b9dae68c0f0ba7efcbb6af8780cbd3
Miguel Amato <miguelamato2812@gmail.com>
2023-09-05 18:40:00 +0200

    Somos estudiantes lets gooooo

---
+++ 42cursus/libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amato <amato@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 18:01:41 by amato             #+#    #+#             */
+/*   Updated: 2023/09/05 18:16:22 by amato            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+
+int	ft_isprint(int c);
+
+#endif

5e75e641b089cdeedfca0c75818a2d12f8dcb738
Miguel Amato <miamato@ucm.es>
2024-01-30 02:10:34 +0100

    odio el gnl

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amato <amato@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 19:28:13 by amato             #+#    #+#             */
-/*   Updated: 2024/01/29 23:01:27 by amato            ###   ########.fr       */
+/*   Updated: 2024/01/30 01:58:39 by amato            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i] != '\0' && s[i] != '\n')
+	while (s[i] != '\0' && s[i] != NL)
 		++i;
 	return (i);
 }
@@ -28,15 +28,15 @@ char	*ft_strdup(char *s, int *i)
 	size_t		size;
 	int			j;
 
-	size = ft_strlen(s + (*i));
+	*i = 0;
+	size = ft_strlen(s);
 	dup = malloc(sizeof(char) * (size + 1));
 	if (dup == 0)
 		return (0);
 	j = 0;
-	while (s[*i] != '\n' && s[*i] != '\0')
+	while (s[*i] != NL && s[*i] != '\0')
 	{
 		dup[j] = s[*i];
-		s[*i] = '\n';
 		++(*i);
 		++j;
 	}
@@ -57,38 +57,60 @@ char	*ft_strjoin(char *s1, char *s2, int *count)
 	j = 0;
 	while (s1[i])
 		ret[j++] = s1[i++];
-	i = 0;
-	while (s2[(*count)] != '\n' && s2[(*count)] != '\0') {
-		ret[j++] = s2[(*count)];
-		s2[(*count)++] = '\n';
+	*count = 0;
+	while (s2[*count] != NL && s2[*count] != '\0') {
+		ret[j++] = s2[*count];
+		(*count)++;
 	}
+	free(s1);
 	ret[j] = '\0';
 	return (ret);
 }
+
+void move_buffer(char *buffer)
+{
+	int	i;
+	int start;
+
+	i = 0;
+	while (buffer[i++] != NL);
+	start = 0;
+	while (buffer[i])
+	{
+		buffer[start] = buffer[i];
+		buffer[i] = '\0';
+		++i;
+		++start;
+	}
+	buffer[start] = '\0';
+}
+
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
 	char		*ret;
 	int			i;
-	if (BUFFER_SIZE <= 0)
+	int			nread;
+
+	if (BUFFER_SIZE <= 0 && fd < 0)
 		return (0);
-	i = 0;
-	while (buffer[i] == '\n')
-		++i;
 	ret = ft_strdup(buffer, &i);
-	if (!ret)
-		return (0);
-	while (i == BUFFER_SIZE || buffer[0] != '\n') 
+	if (ret && buffer[i] != NL)
 	{
-		if (read(fd, buffer, BUFFER_SIZE) == -1)
+		nread = read(fd, buffer, BUFFER_SIZE);
+		while (nread > 0)
 		{
-			free(ret);
-			return (0);
+			buffer[nread] = '\0';
+			ret = ft_strjoin(ret, buffer, &i);
+			if (!ret || buffer[i] == NL)
+				break ;
+			nread = read(fd, buffer, BUFFER_SIZE);
 		}
-		i = 0;
-		ret = ft_strjoin(ret, buffer, &i);
-	}	
+	}
+	if (nread == -1 || nread == 0)
+		return (0);
+	move_buffer(buffer);
 	return (ret);
 }
 
